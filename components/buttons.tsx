cf9af134e599f2720154a818b7a252059c17a3aa
Zachary Piontek <96935362+Zachary-Piontek@users.noreply.github.com>
2023-05-21 19:13:37 -0700

    got auth working, signin/out working

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -1,30 +1,33 @@
-'use client'
+'use client';
 
-import { signIn, signOut, useSession } from "next-auth/react"
-import Image from 'next/image'
-import Link from 'next/link'
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export function SignInButton() {
-    const { data: session, status } = useSession();
-    console.log(session, status);
+  const { data: session, status } = useSession();
+  console.log(session, status);
 
-    if (status === 'loading') {
-        return <>.....</>;
-    }
+  if (status === 'loading') {
+    return <>...</>;
+  }
 
-    if (status === 'authenticated') {
-        return <Link href="/dashboard">
-            <Image src={session.user?.image ?? '/favicon.ico'} alt={session.user?.name ?? 'nope'} width={32} height={32} />
-        </Link>;
-    }
+  if (status === 'authenticated') {
+    return (
+      <Link href={`/dashboard`}>
+        <Image
+          src={session.user?.image ?? '/favicon.ico'}
+          width={32}
+          height={32}
+          alt="Your Name"
+        />
+      </Link>
+    );
+  }
 
-    return <button onClick={() => signIn()}>
-        Sign In
-    </button>;
+  return <button onClick={() => signIn()}>Sign in</button>;
 }
 
 export function SignOutButton() {
-    return <button onClick={() => signOut()}>
-        Sign Out
-    </button>;
+  return <button onClick={() => signOut()}>Sign out</button>;
 }
