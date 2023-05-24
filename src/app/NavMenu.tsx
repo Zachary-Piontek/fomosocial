13e12c452dc6de85167092647ff1a0428073fd53
Zachary Piontek <96935362+Zachary-Piontek@users.noreply.github.com>
2023-05-24 16:38:09 -0700

    added next/themes and dark mode, system works but need to add button functionality

--- src/app/NavMenu.tsx
+++ src/app/NavMenu.tsx
@@ -5,31 +5,36 @@ import { SignInButton, SignOutButton } from "../../components/buttons";
 import AuthCheck from "../../components/AuthCheck";
 
 export default function NavMenu() {
-    return (
-        <nav className={styles.nav}>
-            <Link href={'/'}>
-                <Image src="/chasing-bitcoin-min.png" alt="logo" width={50} height={50} />
-            </Link>
-                <h1>FOMO Social</h1>
-            <ul className={styles.links}>
-                <li>
-                    <Link href="/about">About</Link>
-                </li>
-                <li>
-                    <Link href="/blog">Blog</Link>
-                </li>
-                <li>
-                    <Link href="/users">Users</Link>
-                </li>
-                <li>
-                    <SignInButton />
-                </li>
-                <li>
-                    <AuthCheck>
-                        <SignOutButton />
-                    </AuthCheck>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className={styles.nav}>
+      <Link href={"/"}>
+        <Image
+          src="/chasing-bitcoin-min.png"
+          alt="logo"
+          width={50}
+          height={50}
+        />
+      </Link>
+      <h1>Crypto.Blogs</h1>
+      <ul className={styles.links}>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link href="/users">Users</Link>
+        </li>
+        <li>
+          <SignInButton />
+        </li>
+        <li>
+          <AuthCheck>
+            <SignOutButton />
+          </AuthCheck>
+        </li>
+      </ul>
+    </nav>
+  );
 }
--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,6 +3,5 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
     const users = await prisma.user.findMany();
-    console.log(users);
     return NextResponse.json(users);
 }
