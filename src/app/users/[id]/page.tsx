dd872b8a06642b60dc1005e65a4f57575ee2d773
Zachary Piontek <96935362+Zachary-Piontek@users.noreply.github.com>
2023-05-24 13:52:49 -0700

    small code adjustments

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -3,42 +3,44 @@ import { Metadata } from "next";
 import Image from "next/image";
 
 interface Props {
-    params: {
-        id: string;
-    };
+  params: {
+    id: string;
+  };
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const user = await prisma.user.findUnique({
-        where: {
-            id: params.id,
-        },
-    });
-    return {
-        title: `${user?.name}'s profile`,
-    }
+  const user = await prisma.user.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+  return {
+    title: `${user?.name}'s profile`,
+  };
 }
 
 export default async function UserProfile({ params }: Props) {
-    const user = await prisma.user.findUnique({
-        where: {
-            id: params.id,
-        },
-    });
-    const { name, bio, image } = user ?? {};
-    console.log(bio);
+  const user = await prisma.user.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
 
-    return (
-        <main>
-            <h1>{name}</h1>
-            <Image
-                src={image ?? "/favicon.ico"}
-                alt={`${name}'s profile`}
-                width={200}
-                height={200}
-            />
-            <h2>Bio</h2>
-            <p>{bio}</p>
-        </main>
-    );
+  const { name, bio, image } = user ?? {};
+
+  return (
+    <main>
+      <div>
+        <h1>{name}</h1>
+        <Image
+          src={image ?? "/favicon.ico"}
+          alt={`${name}'s profile`}
+          width={200}
+          height={200}
+        />
+        <h2>Bio</h2>
+        <p>{bio}</p>
+      </div>
+    </main>
+  );
 }
